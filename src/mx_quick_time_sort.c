b7e7c3bfc4854280fd5519cfa12e839ea90f6b46
Selmarinel <yuriy.astahov@gen.tech>
2020-03-20 23:42:07 +0200

    restore t

--- src/mx_quick_time_sort.c
+++ src/mx_quick_time_sort.c
@@ -6,6 +6,22 @@ static void swap_elems(t_elem *arr1, t_elem *arr2) {
     *arr2 = temp;
 }
 
+static int wild_cmp_r(char *s1, char *s2) {
+    int i = 0;
+
+    for (; s1[i] == s2[i] && (s1[i] >= '0' && s1[i] <= '9') && (s2[i] >= '0' && s2[i] <= '9'); i++);
+    if ((s1[i] >= '0' && s1[i] <= '9') || (s2[i] >= '0' && s2[i] <= '9'))
+        return s1[i] - s2[i];
+    else {
+        while (s1[i] == s2[i]) {
+            if (s1[i] == '\0' && s2[i] == '\0')
+                return 0;
+            i++;
+        }
+        return s2[i] - s1[i];
+    }
+}
+
 void mx_quick_time_sort(t_elem **ptr, int left, int right) {
     int i = 0;
     int j = 0;
@@ -16,9 +32,9 @@ void mx_quick_time_sort(t_elem **ptr, int left, int right) {
         i = left;
         j = right;
         while (i < j) {
-            while (mx_strcmp(ptr[i]->f_time, ptr[pivot]->f_time) > 0 && i < right)
+            while (wild_cmp_r(ptr[i]->f_time, ptr[pivot]->f_time) > 0 && i < right)
                 i++;
-            while (mx_strcmp(ptr[j]->f_time, ptr[pivot]->f_time) < 0)
+            while (wild_cmp_r(ptr[j]->f_time, ptr[pivot]->f_time) < 0)
                 j--;
             if (i < j)
                 swap_elems(ptr[i], ptr[j]);
